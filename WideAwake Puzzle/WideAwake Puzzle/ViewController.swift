28c78787c2c44a6be073000ae1d78dc470652479
riptide651 <saloneerege@gmail.com>
2015-11-15 01:55:10 -0800

    Final Movement Engine

--- WideAwake Puzzle/WideAwake Puzzle/ViewController.swift
+++ WideAwake Puzzle/WideAwake Puzzle/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var speedLabel: UILabel!
     @IBOutlet weak var trackButton: UIButton!
 
-    var motionManager: CMMotionManager!
+    var motionManager: CMMotionActivityManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,61 +25,32 @@ class ViewController: UIViewController {
         
     
     @IBAction func startTracking(sender: AnyObject) {
-        motionManager = CMMotionManager()
+        motionManager = CMMotionActivityManager()
         
-        var speedCount: Int
-        speedCount = 0
-        var speedTotal: Double
-        speedTotal = 0
-        var isStanding: Bool
-        var isWalking: Bool
-        isStanding = true
-        isWalking = false
-        //let isJogging = false
-        
-        if motionManager.accelerometerAvailable {
-            motionManager.accelerometerUpdateInterval = 0.05
-            
-            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: { accelerometerData, error in
-                
-                if let currAccel = accelerometerData {
-                    var currSpeed: Double
-                    //var speedCount: Int
+        if(CMMotionActivityManager.isActivityAvailable()){
+            motionManager.startActivityUpdatesToQueue(NSOperationQueue.mainQueue()) { activity in
+                if (activity!.stationary == true) {
+                    self.speedLabel.text = "BUST A MOVE"
+                } else if (activity!.walking == true) {
+                    self.speedLabel.text = "DANCE GOAL MET"
                     
-                    currSpeed = currAccel.acceleration.x//sqrt(currAccel.acceleration.x*currAccel.acceleration.x + currAccel.acceleration.y*currAccel.acceleration.y)
+                    let timer: NSTimer = NSTimer.scheduledTimerWithTimeInterval(3, target: self, selector: Selector("segue:"), userInfo: nil, repeats: false)
                     
-                    speedCount = speedCount+1
-                    speedTotal = speedTotal + currSpeed
-                    
-                    //var isStanding = 0.15
-                    if (speedCount%20 == 0) {
-                        currSpeed = speedTotal/20.0
-                        speedTotal = 0
-                        print(currSpeed)
-                        if (currSpeed > 0.24 && isStanding == true) {
-                            self.speedLabel.text = "CURRENTLY WALKING"
-                            isStanding = false
-                            isWalking = true
-                        }
-                        if (currSpeed < 0.10 && isWalking == true) {
-                            self.speedLabel.text = "CURRENTLY STANDING"
-                            isStanding = true
-                            isWalking = false
-                        }
-                    }
                 }
-                
-                
-            })
-            
-            
+            }
         }
-    }
+
+}
 
     @IBAction func stopTracking(sender: AnyObject) {
-        motionManager.stopAccelerometerUpdates()
-        self.speedLabel.text = "CURRENTLY STANDING"
+        motionManager.stopActivityUpdates()
+        self.speedLabel.text = "BUST A MOVE"
         
+    }
+    
+    func segue(timer : NSTimer) {
+        //segue code
+        print("Timer fired")
     }
     
     override func didReceiveMemoryWarning() {
@@ -89,6 +60,10 @@ class ViewController: UIViewController {
     }
 }
 
+
+
+
+//Stuff
 
 
 
